e36a4a6cb6fac186ca6cacbe6cdf1442f270b8f5
Meshech Johnathan <meshechjohnathan@icloud.com>
2026-02-17 20:27:33 +0530

    mongodb connect

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//load environment variables
+dotenv.config();
+
+//create express app
+const app = express();
+
+//middleware to parse JSON bodies
+app.use(express.json());
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected ✅'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+//define a simple route
+app.get("/", (req,res ) => {
+  res.send("API is running...");
+})
+
+//start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT,() => {
+  console.log(`Server is running on port ${PORT}`);
+});
