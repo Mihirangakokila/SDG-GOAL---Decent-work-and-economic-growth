ec2230f81651423a61008af868b75b8537b30431
Fathima Aathika <atkilmdn1125@gmail.com>
2026-02-24 06:05:51 +0530

    Added internship get method

--- backend/src/controllers/internshipController.js
+++ backend/src/controllers/internshipController.js
@@ -1,4 +1,10 @@
-import { createInternship } from "../services/internshipService.js";
+import { 
+  createInternship ,
+  updateInternship,
+  deleteInternship,
+  getInternshipByIdController,
+  getMyInternships
+} from "../services/internshipService.js";
 
 // Controller to handle internship creation
 export const createInternshipController = async (req, res) => {
@@ -13,4 +19,52 @@ export const createInternshipController = async (req, res) => {
   }
     
   };
-  
+
+  // Controller to handle internship update
+export const updateInternshipController = async (req, res) => {
+  try{  
+    const internship = await updateInternship(
+      req.params.id,
+      req.user.id,
+      req.body
+    );  
+
+    res.json(internship);
+  } catch(error){
+    res.status(500).json({message : error.message});
+  }
+};
+
+// Controller to handle internship deletion
+export const deleteInternshipController = async (req, res) => {
+  try{
+    const internship = await deleteInternship(
+      req.params.id,
+      req.user.id
+    );  
+    res.json({message : "Internship deleted successfully"});
+  } catch(error){
+    res.status(500).json({message : error.message});
+  } 
+};
+
+//Get Single Internship
+export const getInternshipByIdController = async (req, res) => {
+  try {
+    const internship = await getInternshipByIdService(req.params.id);
+    res.json(internship);
+  } catch(error){
+    res.status(500).json({message : error.message});
+  }
+};
+
+//Get My Internships (Organization)
+export const getMyInternships  = async (req, res) => {
+  try {
+    const internships = await getMyInternshipsService(req.user.id);   
+    res.json(internships);
+  }
+    catch(error){ 
+    res.status(500).json({message : error.message});
+  }
+};
