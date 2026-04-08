0051af5152f6e530f126352a382db3701d97ed32
Fathima Aathika <atkilmdn1125@gmail.com>
2026-04-08 20:22:34 +0530

    fix: added correct model structure

---
+++ backend/src/models/internship.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose"; 
+
+const internshipSchema = new mongoose.Schema({
+  tittle : {
+    type : String,
+    required :true
+  },
+  description :{
+    type : String,
+    required: true
+  },
+  requiredSkills : [
+    {
+      type : String
+    }
+  ],
+  requiredEducation : {
+    type : String 
+  },
+  location: {
+  type: String
+},
+coordinates: {
+  type: {
+    type: String,
+    enum: ["Point"],
+    default: "Point"
+  },
+  coordinates: {
+    type: [Number], // [longitude, latitude]
+    default: undefined
+  }
+},
+  duration : {
+    type : String
+  },
+  status :{
+    type: String,
+    enum :["Draft","Active","Closed"],
+    default : "Draft"
+  },
+  organizationId :{
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "User",
+    required : true
+  },
+  totalapplicants : {
+    type : Number,
+    default : 0 
+  },
+  acceptedCount :{
+    type : Number,
+    default : 0
+  },
+  viewCount : {
+    type : Number,
+    default : 0
+  }
+  },{
+    timestamps : true
+  }
+);
+
+internshipSchema.index({ coordinates: "2dsphere" });
+
+export default mongoose.models.Internship || mongoose.model("Internship", internshipSchema);
